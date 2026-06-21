1d3584d781b76dae7e6bb64f734ac017620eda3e
LourencoFernando <gastronumo@gmail.com>
2026-06-21 13:57:46 +0100

    Added Navigation Icon to TopBar

--- app/src/main/java/com/example/wishflow/AppBar.kt
+++ app/src/main/java/com/example/wishflow/AppBar.kt
@@ -2,12 +2,17 @@ package com.example.wishflow
 
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 
@@ -26,6 +31,17 @@ fun AppBarView(
             )},
         colors = TopAppBarDefaults.topAppBarColors(
             containerColor = colorResource(id = R.color.app_bar_color)
-        )
+        ),
+        navigationIcon = {
+            if (!title.contains("WishFlow")) {
+                IconButton(onClick = { onBackNavClicked() }) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "Back",
+                        tint = Color.White
+                    )
+                }
+            }
+        }
     )
 }
--- app/src/main/java/com/example/wishflow/HomeView.kt
+++ app/src/main/java/com/example/wishflow/HomeView.kt
@@ -1,17 +1,22 @@
 package com.example.wishflow
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 
 @Composable
 fun HomeView() {
+    val context = LocalContext.current
 
     Scaffold(
-        topBar = { AppBarView(title = "WishFlow") }
+        topBar = { AppBarView(title = "WishFlow", {
+            Toast.makeText(context, "Back button clicked", Toast.LENGTH_SHORT).show()
+        }) }
     ) {
         LazyColumn(modifier = Modifier.fillMaxSize().padding(it)) {
 
