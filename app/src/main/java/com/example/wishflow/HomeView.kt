27412ab34ee3e923e7360bc76a81213abe459c8f
LourencoFernando <gastronumo@gmail.com>
2026-06-21 13:19:17 +0100

    Implemented Top App Bar

---
+++ app/src/main/java/com/example/wishflow/HomeView.kt
@@ -0,0 +1,21 @@
+package com.example.wishflow
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HomeView() {
+
+    Scaffold(
+        topBar = { AppBarView(title = "WishFlow") }
+    ) {
+        LazyColumn(modifier = Modifier.fillMaxSize().padding(it)) {
+
+        }
+    }
+
+}
