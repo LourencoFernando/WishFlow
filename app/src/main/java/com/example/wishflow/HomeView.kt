b964ad1e63e2edb1bae490e54bd16d4e02c6ff40
LourencoFernando <gastronumo@gmail.com>
2026-06-21 14:07:38 +0100

    Added Floating Action Button

--- app/src/main/java/com/example/wishflow/HomeView.kt
+++ app/src/main/java/com/example/wishflow/HomeView.kt
@@ -4,10 +4,16 @@ import android.widget.Toast
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeView() {
@@ -16,7 +22,19 @@ fun HomeView() {
     Scaffold(
         topBar = { AppBarView(title = "WishFlow", {
             Toast.makeText(context, "Back button clicked", Toast.LENGTH_SHORT).show()
-        }) }
+        }) },
+        floatingActionButton = {
+            FloatingActionButton(
+                modifier = Modifier.padding(all = 20.dp),
+                contentColor = Color.White,
+                containerColor = Color.Black,
+                onClick = {
+                    Toast.makeText(context, "FAB button clicked", Toast.LENGTH_SHORT).show()
+                }
+            ) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Add")
+            }
+        }
     ) {
         LazyColumn(modifier = Modifier.fillMaxSize().padding(it)) {
 
