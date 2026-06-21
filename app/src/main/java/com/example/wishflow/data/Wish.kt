3a9d14a21b1ae29d2bdafbc623fe2ec3978d88b0
LourencoFernando <gastronumo@gmail.com>
2026-06-21 18:26:30 +0100

    Setup @Entity for Wish Data Class

--- app/src/main/java/com/example/wishflow/data/Wish.kt
+++ app/src/main/java/com/example/wishflow/data/Wish.kt
@@ -1,8 +1,16 @@
 package com.example.wishflow.data
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "wish_table")
 data class Wish(
+    @PrimaryKey(autoGenerate = true)
     val id: Long = 0L,
+    @ColumnInfo(name = "wish_title")
     val title: String="",
+    @ColumnInfo(name = "wish_description")
     val description: String="",
 )
 
@@ -12,10 +20,6 @@ object DummyWish {
         Wish(2L, "Wish 2", "Description 2"),
         Wish(3L, "Wish 3", "Description 3"),
         Wish(4L, "Wish 4", "Description 4"),
-        Wish(5L, "Wish 5", "Description 5"),
-        Wish(6L, "Wish 6", "Description 6"),
-        Wish(7L, "Wish 7", "Description 7"),
-        Wish(8L, "Wish 8", "Description 8"),
-        Wish(9L, "Wish 9", "Description 9")
+        Wish(5L, "Wish 5", "Description 5")
     )
 }
